6acc6652f452e9a9d3048fc208d3d9c3fbdf6338
filipepre <49457814+filipepre@users.noreply.github.com>
2021-03-14 14:40:10 +0000

    V0.3
    V0.3

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // create an express app
 const express = require("express")
 const app = express()
+var path = require('path');
 
 // use the express-static middleware
 app.use(express.static("public"))
@@ -9,6 +10,14 @@ app.use(express.static("public"))
 app.get("/", function (req, res) {
   res.send("<h1>Hello World!</h1>")
 })
+
+app.get("/test", function (req, res) {
+  res.sendFile(path.join(__dirname + '/app/main/page.html'))
+});
+
+app.get("/bg", function (req, res) {
+  res.sendFile(path.join(__dirname + '/app/main/bg.jpg'))
+});
 
 // start the server listening for requests
 app.listen(process.env.PORT || 3000, 
